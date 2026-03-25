c291e2d4660283e6a81e46daef40194d03cdf3a4
Miroslav Kovacevic <miroslav.kovacevic@eng.it>
2026-03-25 16:52:23 +0100

    Liquibase dependency and changeSets for roles and users_roles tables added

--- auth-service/src/main/java/it/eng/auth_service/configs/ApplicationConfiguration.java
+++ auth-service/src/main/java/it/eng/auth_service/configs/ApplicationConfiguration.java
@@ -2,10 +2,13 @@ package it.eng.auth_service.configs;
 
 
 
+import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
 
 
 @Configuration
@@ -16,5 +19,11 @@ public class ApplicationConfiguration {
         return new BCryptPasswordEncoder();
     }
 
-
+    @Bean
+    public SpringLiquibase liquibase(DataSource dataSource) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setDataSource(dataSource);
+        liquibase.setChangeLog("classpath:db/changelog/db.changelog-master.xml");
+        return liquibase;
+    }
 }
